ac8e184b559e9011cf5d93dff5628aa8f2a67ea9
DesKramer <destkramers@gmail.com>
2021-02-06 15:02:02 +0000

    Changed server prot

--- server/api/app.js
+++ server/api/app.js
@@ -9,5 +9,5 @@ app.use(cors());
 app.use("/", routes);
 
 // Turn on that server!
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
