c018e21860a28eb66094433e7eeec2291de9c1b0
dan-frank <dan.frank.lucas@gmail.com>
2021-02-06 13:18:18 +0000

    Setup API connection to mongodb

--- server/api/app.js
+++ server/api/app.js
@@ -2,8 +2,22 @@
 const app = require("express")();
 const routes = require("./routes");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(cors());
+
+const mongodbPassword = "admin123";
+const mongodDatabase = "testdb";
+const uri = `mongodb+srv://admin:${mongodbPassword}@cluster0.qpu8j.mongodb.net/${mongodDatabase}?retryWrites=true&w=majority`;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected…");
+  })
+  .catch((err) => console.log(err));
 
 //  Connect all our routes to our application
 app.use("/", routes);
