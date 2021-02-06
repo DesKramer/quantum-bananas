aaabb7b5bd8c5e98b8266cd031f2e0aec73be02f
DesKramer <destkramers@gmail.com>
2021-02-06 18:20:35 +0000

    Updated Connector.js

--- raspberry-pi/connector.py
+++ raspberry-pi/connector.py
@@ -1,33 +1,39 @@
 import requests
+import json
 
 class Connector:
     def checkStatus(self):
         r = requests.get(self.url)
         return r.status_code
 
-    def postAlert(self, objects):
-        print("")
-        # Requires endpoint to post to
 
+    def getKitchens(self):
+        r = requests.get(self.url);
+        print(r.json())
+
+
+    def postKitchen(self, objects):
+       
+        headers = {'Content-Type' : 'application/json'}
         isMessy = False
-
 
         if(len(objects) > 0):
             isMessy = True
 
-
         payload = {
-            'kitchen' : 'main_kitchen',
-            'messy' : isMessy,
+            'kitchen' : "main_kitchen",
+            'messy' : 'true',
             'objects' : objects,
             }
-        r = requests.post(url+'api/alerts', data=payload)
-
-
+        r = requests.post(self.url, headers=headers, json=payload)
+        print(r.status_code)
 
     def __init__(self):
-        self.url = 'http://localhost:5000/'
+        self.url = 'http://localhost:5000/kitchens/'
 
 
 con = Connector()
-print(con.checkStatus())
+# print(con.checkStatus())
+items = "Glass, Pot, Pan, Fork, Can, Plate"
+con.postKitchen(items)
+# con.getKitchens()
