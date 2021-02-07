b2c20caf3c20b3cbaf4d6890766c7122314d5cfb
DesKramer <destkramers@gmail.com>
2021-02-07 17:40:44 +0000

    Update driver.py

--- raspberry-pi/driver.py
+++ raspberry-pi/driver.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -5,88 +6,101 @@ import sys
 import tarfile
 import tensorflow as tf
 import zipfile
-import pathlib
+
+from distutils.version import StrictVersion
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
-from IPython.display import display
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
 from object_detection.utils import ops as utils_ops
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
- 
- 
-# PATH_TO_LABELS = 'labelmap.pbtxt'
-# category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
- 
-model_name = 'inference_graph'
+
+if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
+
+
+
+MODEL_NAME = 'inference_graph'
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
 PATH_TO_LABELS = 'training/labelmap.pbtxt'
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
+def run_inference_for_single_image(image, graph):      
+    if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-def run_inference_for_single_image(model, image):
-  image = np.asarray(image)
-  # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
-  input_tensor = tf.convert_to_tensor(image)
-  # The model expects a batch of images, so add an axis with `tf.newaxis`.
-  input_tensor = input_tensor[tf.newaxis,...]
- 
-  # Run inference
-  model_fn = model.signatures['serving_default']
-  output_dict = model_fn(input_tensor)
- 
-  # All outputs are batches tensors.
-  # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-  # We're only interested in the first num_detections.
-  num_detections = int(output_dict.pop('num_detections'))
-  output_dict = {key:value[0, :num_detections].numpy() 
-                 for key,value in output_dict.items()}
-  output_dict['num_detections'] = num_detections
- 
-  # detection_classes should be ints.
-  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-    
-  # Handle models with masks:
-  if 'detection_masks' in output_dict:
-    # Reframe the the bbox mask to the image size.
-    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-              output_dict['detection_masks'], output_dict['detection_boxes'],
-               image.shape[0], image.shape[1])      
-    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                       tf.uint8)
-    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
-     
-  return output_dict
+    # Run inference
+    output_dict = sess.run(tensor_dict,
+                           feed_dict={image_tensor: np.expand_dims(image, 0)})
 
-def show_inference(model, frame):
-  #take the frame from webcam feed and convert that to array
-  image_np = np.array(frame)
-  # Actual detection.
-     
-  output_dict = run_inference_for_single_image(model, image_np)
-  # Visualization of the results of a detection.
-  vis_util.visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      instance_masks=output_dict.get('detection_masks_reframed', None),
-      use_normalized_coordinates=True,
-      line_thickness=5)
- 
-  return(image_np)
+    # all outputs are float32 numpy arrays, so convert types as appropriate
+    output_dict['num_detections'] = int(output_dict['num_detections'][0])
+    output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+    if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
+  
 
-# Webcam - Object Detection
 import cv2
-video_capture = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-while(True):
-    re,frame = video_capture.read()
-    Image_np=show_inference(detection_model,frame)
-    cv2.imshow('object detection', cv2.resize(frame, (800,600)))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
+with detection_graph.as_default():
+    with tf.Session() as sess:
+        ops = tf.get_default_graph().get_operations()
+        all_tensor_names = {output.name for op in ops for output in op.outputs}
+        tensor_dict = {}
+        for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+          ]:
+            tensor_name = key + ':0'
+            if tensor_name in all_tensor_names:
+                tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+        while True:
+            re,frame = cap.read()
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(frame, axis=0)
+            # Actual detection.
+            output_dict = run_inference_for_single_image(frame, detection_graph)
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+              image_np,
+              output_dict['detection_boxes'],
+              output_dict['detection_classes'],
+              output_dict['detection_scores'],
+              category_index,
+              instance_masks=output_dict.get('detection_masks'),
+              use_normalized_coordinates=True,
+              line_thickness=8)
+            cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cap.release()
+                cv2.destroyAllWindows()
+                break
