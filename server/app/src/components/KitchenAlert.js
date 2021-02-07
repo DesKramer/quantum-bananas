fa400af5262a792d0b35cd91dcba8a92ace057c3
dan-frank <dan.frank.lucas@gmail.com>
2021-02-07 14:59:42 +0000

    Refactored KitchenAlert

--- server/app/src/components/KitchenAlert.js
+++ server/app/src/components/KitchenAlert.js
@@ -10,51 +10,48 @@ const KitchenAlert = () => {
     });
   }, []);
 
-  if (Object.keys(item).length !== 0) {
-    const objects = item.objects.split(",");
-
-    return (
-      <div className="kitchen__latest">
-        <p>
-          {item.messy
-            ? "AI Kitchen Helper detects a mess"
-            : "AI Kitchen Helper detects no mess"}
-          {item.kitchen ? (
-            <Fragment>
-              {" "}
-              in
-              <span className="is-bold"> {item.kitchen}</span>
-            </Fragment>
-          ) : (
-            ""
-          )}
-          !
-        </p>
-        {objects[0] !== "" ? (
-          <Fragment>
-            <p>You NEED to clean the following:</p>
-            <ul className="kitchen__list">
-              {objects.map((object, index) => {
-                return (
-                  <li key={index} className="kitchen__list__item">
-                    {object}
-                  </li>
-                );
-              })}
-            </ul>
-          </Fragment>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  } else {
+  if (Object.keys(item).length === 0) {
     return (
       <div>
         <h3>Please run AI Kitchen Helper...</h3>
       </div>
     );
   }
+
+  const objects = item.objects.split(",");
+
+  return (
+    <div className="kitchen__latest">
+      <p>
+        {item.messy ? "We've detected a mess" : "We've detected no mess"}
+        {!!item.kitchen && (
+          <Fragment>
+            {" in"}
+            <span className="is-bold"> {item.kitchen}</span>
+            {" kitchen"}
+          </Fragment>
+        )}
+        !
+      </p>
+      {objects[0] !== "" && (
+        <Fragment>
+          <p>You NEED to clean the following:</p>
+          <ul className="kitchen__list">
+            {objects.map((object, index) => {
+              return (
+                <li
+                  key={"name--" + object.replace(/\s/g, "")}
+                  className="kitchen__list__item"
+                >
+                  {object}
+                </li>
+              );
+            })}
+          </ul>
+        </Fragment>
+      )}
+    </div>
+  );
 };
 
 export default KitchenAlert;
