a708a665b1d2a9124db9019309fdb3d06bf32697
dan-frank <dan.frank.lucas@gmail.com>
2021-02-07 15:09:43 +0000

    Renamed io -> socketIo for readability

--- server/app/src/components/KitchenAlert.js
+++ server/app/src/components/KitchenAlert.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
-import { latestKitchen } from "../io";
+import { latestKitchen } from "../socketIo";
 
 const KitchenAlert = () => {
   const [item, updateItem] = useState({});
